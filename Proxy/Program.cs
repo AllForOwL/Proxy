5ccd43195a2599eabfbf8daa1a89cd253c039a3b
OleksandrGavrisyuk <sahavrysyuk@gmail.com>
2017-07-13 21:21:17 +0300

    Initialize Project

---
+++ Proxy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timeNow = DateTime.Now;
+
+            var people = new People(new ProxyThink(new Think()));
+
+            if (timeNow.Hour == 9)
+                people.WakeUp();
+
+            else
+                people.InWork();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Proxy/Proxy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Proxy
+{
+    public class People
+    {
+        private ProxyThink _think;
+
+        public People(ProxyThink think)
+        {
+            _think = think;
+        }
+
+        public void WakeUp()
+        {
+            _think.ThinkSmall();
+        }
+
+        public void InWork()
+        {
+            _think.ThinkDeep();
+        }
+    }
+
+    public interface IThink
+    {
+        void ThinkDeep();
+        void ThinkSmall();
+    }
+
+    public class Think : IThink
+    {
+        private string _veryBigObject;
+
+        public void ThinkDeep()
+        {
+            if (_veryBigObject != null)
+                Console.WriteLine("I think " + _veryBigObject);
+            else
+            {
+                _veryBigObject = ". As very big object";
+                Console.WriteLine("I think deep" + _veryBigObject);
+            }
+        }
+
+        public void ThinkSmall()
+        {
+            Console.WriteLine("I thing very small");
+        }
+    }
+
+    public class ProxyThink : IThink
+    {
+        private readonly Think _think;
+
+        public ProxyThink(Think think)
+        {
+            _think = think;
+        }
+
+        public void ThinkDeep()
+        {
+            _think.ThinkDeep();
+        }
+
+        public void ThinkSmall()
+        {
+            _think.ThinkSmall();
+        }
+    }
+}
